9a77b60dec3a66f1b9faf1367b0bf41f0a2588fd
OussamaELhansali <102356792+OussamaELhansali@users.noreply.github.com>
2023-05-20 15:45:04 +0200

    Rock-papper-scissors & Hang-Man projects

---
+++ Rock-papper-scissors.py
@@ -0,0 +1,44 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+l=[rock,paper, scissors]
+choice= int(input('What do you chose? 0 for Rock, 1 for papper and 2 for scissors.'))
+choice_1= random.randint(0,2)
+if choice_1 == choice+1 or  choice_1 == choice -2:
+  print(l[choice])
+  print(f'\n computer chose\n {l[choice_1]}\n')
+  print('You lose!')
+if choice_1 == choice -1 or choice == choice_1 -2:
+  print(l[choice])
+  print(f'\n computer chose\n {l[choice_1]}\n')
+  print('You win!')
+elif choice == choice_1:
+  print(l[choice])
+  print(f'\n computer chose\n {l[choice_1]}\n')
+  print("It's a Draw!") 
